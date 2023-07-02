5702d2a260869603f9a51c8d382e33271dd7eda4
sereedmedia <se@kerredyn.com>
2023-07-01 18:10:05 -0700

    Updated marketing team description

--- src/src/WPCOTool/config/teams.php
+++ src/src/WPCOTool/config/teams.php
@@ -29,7 +29,7 @@ return array(
 	),
 	'marketing' => array(
 		'name'          => esc_html__( 'Marketing', 'contributor-orientation-tool' ),
-		'description'   => esc_html__( 'Our vision for the Marketing Team is to be the go-to resource for strategy and content for other WordPress teams.', 'contributor-orientation-tool' ),
+		'description'   => esc_html__( 'The Marketing team promotes WordPress to current and future users and contributors. We create and amplify campaigns to support the growth of the WordPress project.', 'contributor-orientation-tool' ),
 		'icon'          => '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><rect x="0" fill="none" width="20" height="20"/><g><path d="M10 1c7 0 9 2.91 9 6.5S17 14 10 14s-9-2.91-9-6.5S3 1 10 1zM5.5 9C6.33 9 7 8.33 7 7.5S6.33 6 5.5 6 4 6.67 4 7.5 4.67 9 5.5 9zM10 9c.83 0 1.5-.67 1.5-1.5S10.83 6 10 6s-1.5.67-1.5 1.5S9.17 9 10 9zm4.5 0c.83 0 1.5-.67 1.5-1.5S15.33 6 14.5 6 13 6.67 13 7.5 13.67 9 14.5 9zM6 14.5c.83 0 1.5.67 1.5 1.5s-.67 1.5-1.5 1.5-1.5-.67-1.5-1.5.67-1.5 1.5-1.5zm-3 2c.55 0 1 .45 1 1s-.45 1-1 1-1-.45-1-1 .45-1 1-1z"/></g></svg>',
 		'url'           => 'https://make.wordpress.org/marketing/handbook/getting-involved/',
 	),
